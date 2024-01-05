c138d02ebf5efcdec982995310fa00439ac18686
JohnLucid1 <SobolII21@st.ithub.ru>
2024-01-05 19:36:17 +0300

    added cli functionality and search by types

--- src/main.v
+++ src/main.v
@@ -6,22 +6,31 @@
 module main
 
 import os
+import math
+// import encoding.utf8
 import v.parser
+import arrays
 import v.ast
 import v.pref
+const cli_max = 10
 
 // TODO: using treesitter SQL tables to go structs with json
 // TODO: concurently search folders
 // TODO: maybe create a mutex vec to add to it concurentrly
 // TODO: How the fuck do you get return type of structs instead of fn (voidptr) bool
-
 struct FnSignature {
 	name        string
 	file_path   string
 	return_type string
 	pos         int
-	col         int
+	ln int
 	params      []Param
+mut:
+	acc int = -1
+}
+
+fn (v FnSignature) println() {
+	println('${v.file_path}${v.pos}${v.ln} $: ${v.name} :: ${v.params.map(it.type_)} score: ${v.acc}')
 }
 
 struct Param {
@@ -29,23 +38,84 @@ struct Param {
 	is_mut bool
 }
 
-// TODO: Walk a directory, get each file that ends with .v, and parse them :) 
+fn calc_distance(str1 string, str2 string) int {
+	m := str1.len
+	n := str2.len
+	highest := math.max(n, m)
+	mut dp := [][]int{len: highest + 1, init: []int{len: highest + 1}}
+
+	for i := 0; i <= m; i++ {
+		dp[i][0] = i
+	}
+
+	for j := 0; j <= n; j++ {
+		dp[0][j] = j
+	}
+
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if str1[i - 1] == str2[j - 1] {
+				dp[i][j] = dp[i - 1][j - 1]
+			} else {
+				dp[i][j] = 1 + math.min(dp[i][j - 1], math.min(dp[i - 1][j], dp[i - 1][j - 1]))
+			}
+		}
+	}
+
+	return dp[m][n]
+}
+
 // THEN Serailize them with json
 // Implement flag module in it
 // Depenant on the flag, re-serialize directory or not
-
+// TODO: make it a flag to search by cli
 fn main() {
 	// FIXME: This is a test of data, in future write a function that pulls all files with .v from directory that you get from cmd arguments
 	// TODO: actually implement flags
+	// start_directory := os.args[1]
+	// filepaths := os.walk_ext(start_directory, '.v')
+	// if filepaths.len == 0 {
+	// 	eprintln("Starting directory doesn't contain any .v files")
+	// 	return
+	// }
+
+	// for filepath in filepaths {
+	// 	if !os.exists(filepath) {
+	// 		eprintln('FILEPAHT DOES NOT EXIST')
+	// 		return
+	// 	}
+	// }
+
+	// table := ast.new_table()
+	// prefs := pref.new_preferences()
+	// _ := parser.parse_files(filepaths, table, prefs)
+
+	// mut all_singnatures := []FnSignature{}
+	// for _, fn_signature in table.fns {
+	// 	res := parse_sig(fn_signature, table) or {
+	// 		eprintln(err)
+	// 		return
+	// 	}
+	// 	all_singnatures << res
+	// }
+
+	// all_singnatures.println()
+	// println(calc_distance("main.TEst_some1", "main.test_some1"))
+
+	// TEST: this is a test of cli
+
 	start_directory := os.args[1]
-	filepaths := os.walk_ext(start_directory, ".v")
-	if filepaths.len  == 0 {
+	input := os.args[2]
+	filepaths := os.walk_ext(start_directory, '.v')
+
+	if filepaths.len == 0 {
 		eprintln("Starting directory doesn't contain any .v files")
-		return 
+		return
 	}
 
 	for filepath in filepaths {
 		if !os.exists(filepath) {
+			println('happend there')
 			eprintln('FILEPAHT DOES NOT EXIST')
 			return
 		}
@@ -57,37 +127,70 @@ fn main() {
 
 	mut all_singnatures := []FnSignature{}
 	for _, fn_signature in table.fns {
-		res := parse_sig(fn_signature, table) or {
-			eprintln(err)
-			return
-		}
+		res := parse_sig(fn_signature, table) or { continue }
 		all_singnatures << res
 	}
-
-	all_singnatures.println()
+	parse_cli(input, mut all_singnatures)
 }
 
+// TODO: Write init function that takes care of tables and so on
+fn parse_cli(search_str string, mut fn_singatures []FnSignature) ? {
+	ret, types := parse_input(search_str)
+	println('ret: ${ret}, types: ${types}')
+	parsed_types := types.trim("()").split(",")
 
-fn (sigs []FnSignature) println() {
-	for sig in sigs {
-		println("\n$sig.file_path, $sig.name, $sig.return_type\n$sig.params")
+	for mut sig in fn_singatures {
+		ret_dist := calc_distance(ret, sig.return_type)
+		types_dist := calc_array(parsed_types, sig.params.map(it.type_))
+		sig.acc = ret_dist + types_dist
+	}
+
+	fn_singatures.sort(a.acc < b.acc)
+
+	for i in 0 .. cli_max {
+		fn_singatures[i].println()
 	}
 }
 
-// TODO: SHould get type from ast
-// TODO: write a cli that searches types:
-// TODO: Test if i even need to change struct type to Person from main.Person
+fn calc_array(arr1 []string, arr2[]string) int {
+	mut dist := 0
+	if arr1.len == arr2.len {
+		for i, _ in arr1 {
+			dist += calc_distance(arr1[i], arr2[i])	
+		}
+	}else {
+		if arr1.len > arr2.len{
+			for i, _ in arr2 {
+				dist += calc_distance(arr1[i], arr2[i])	
+			}			
+		}else {
+			for i, _ in arr1 {
+				dist += calc_distance(arr1[i], arr2[i])	
+			}			
+		}
+	}
+	return dist
+}
+
+fn parse_input(search_str string) (string, string) {
+	ret, types := search_str.split_once(' ') or { return '', '' }
+	return ret, types
+}
+
 fn parse_sig(sig ast.Fn, table &ast.Table) ?FnSignature {
+	if sig.name == 'main.main' {
+		return none
+	}
 	toip := FnSignature{
 		params: sig.params.map(Param{
 			is_mut: it.is_mut
-			type_: table.get_type_name(it.typ)
+			type_: table.get_type_name(it.typ).to_lower()
 		})
 		file_path: sig.file
 		pos: sig.pos.pos
-		col: sig.pos.col
-		name: sig.name
-		return_type: table.get_type_name(sig.return_type)
+		ln: sig.pos.line_nr
+		name: sig.name.to_lower()
+		return_type: table.get_type_name(sig.return_type).to_lower()
 	}
 	return toip
 }
--- src/test.v
+++ src/test.v
@@ -11,21 +11,22 @@ fn test_some2(n1 string, n2 string) (string, string) {
 }
 
 fn (mut val Person) add_age() {
-	val.age ++
+	val.age++
 }
 
 fn add_age(mut p Person) {
 	p.age++
 }
 
-
-fn create_person(name string, age int) Person{
-	return Person{name: name, age: age}
+fn create_person(name string, age int) Person {
+	return Person{
+		name: name
+		age: age
+	}
 }
 
 struct Person {
-	mut: 
+mut:
 	name string
-	age int
+	age  int
 }
-
